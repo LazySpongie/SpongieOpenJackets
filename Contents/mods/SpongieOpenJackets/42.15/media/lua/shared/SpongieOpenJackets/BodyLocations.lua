74828d8174dfd34510473ad5c1a37be54a722f85
Spongie <lazyspongie@gmail.com>
2026-03-11 20:20:51 +0000

    bodylocations not tested

---
+++ Contents/mods/SpongieOpenJackets/42.15/media/lua/shared/SpongieOpenJackets/BodyLocations.lua
@@ -0,0 +1,82 @@
+
+
+local BodyLocations_Helper = require("SpongieOpenJackets/BodyLocations_Helper")
+
+local group = BodyLocations.getGroup("Human")
+local jacketIndex = group:indexOf(ItemBodyLocation.JACKET)
+
+local bodylocations = {
+    [SpnOpenCloth.ItemBodyLocation.JACKET_OPEN] = {
+        index = jacketIndex,
+        exclusive = {
+            SpnOpenCloth.ItemBodyLocation.JACKET_ROLL,
+            ItemBodyLocation.FULL_SUIT_HEAD,
+            ItemBodyLocation.FULL_SUIT,
+            ItemBodyLocation.FULL_TOP,
+            ItemBodyLocation.BATH_ROBE,
+            ItemBodyLocation.FULL_ROBE,
+            ItemBodyLocation.JACKET_DOWN,
+            ItemBodyLocation.JACKET_HAT,
+            ItemBodyLocation.JACKET_BULKY,
+            ItemBodyLocation.JACKET_HAT_BULKY,
+            ItemBodyLocation.JACKET_SUIT,
+            ItemBodyLocation.SPORT_SHOULDERPAD,
+            ItemBodyLocation.TORSO_EXTRA,
+        },
+        hidden = {
+            ItemBodyLocation.LEFT_WRIST,
+            ItemBodyLocation.RIGHT_WRIST,
+            ItemBodyLocation.FANNY_PACK_FRONT,
+            ItemBodyLocation.FANNY_PACK_BACK,
+            ItemBodyLocation.SHOULDER_HOLSTER,
+        },
+        altModel = {
+            ItemBodyLocation.FORE_ARM_LEFT,
+            ItemBodyLocation.FORE_ARM_RIGHT,
+            ItemBodyLocation.SHOULDERPAD_RIGHT,
+            ItemBodyLocation.SHOULDERPAD_LEFT,
+            ItemBodyLocation.ELBOW_LEFT,
+            ItemBodyLocation.ELBOW_RIGHT,
+            ItemBodyLocation.CUIRASS,
+            ItemBodyLocation.WEBBING,
+        },
+    },
+    [SpnOpenCloth.ItemBodyLocation.JACKET_ROLL] = {
+        index = jacketIndex,
+        exclusive = {
+            SpnOpenCloth.ItemBodyLocation.JACKET_OPEN,
+            ItemBodyLocation.FULL_SUIT_HEAD,
+            ItemBodyLocation.FULL_SUIT,
+            ItemBodyLocation.FULL_TOP,
+            ItemBodyLocation.BATH_ROBE,
+            ItemBodyLocation.FULL_ROBE,
+            ItemBodyLocation.JACKET_DOWN,
+            ItemBodyLocation.JACKET_HAT,
+            ItemBodyLocation.JACKET_BULKY,
+            ItemBodyLocation.JACKET_HAT_BULKY,
+            ItemBodyLocation.JACKET_SUIT,
+            ItemBodyLocation.SPORT_SHOULDERPAD,
+            ItemBodyLocation.TORSO_EXTRA,
+        },
+        hidden = {
+            ItemBodyLocation.FANNY_PACK_FRONT,
+            ItemBodyLocation.FANNY_PACK_BACK,
+            ItemBodyLocation.SHOULDER_HOLSTER,
+        },
+        altModel = {
+            ItemBodyLocation.SHOULDERPAD_RIGHT,
+            ItemBodyLocation.SHOULDERPAD_LEFT,
+            ItemBodyLocation.ELBOW_LEFT,
+            ItemBodyLocation.ELBOW_RIGHT,
+            ItemBodyLocation.CUIRASS,
+            ItemBodyLocation.WEBBING,
+        },
+    },
+}
+
+
+for name, data in pairs(bodylocations) do
+    BodyLocations_Helper.CreateLocation(name, data)
+end
+
+bodylocations = nil
